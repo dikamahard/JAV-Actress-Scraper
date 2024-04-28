347a220229b6ed76862fb5c423fd1f762ed0c987
Dikamahard <ikkyboss@gmail.com>
2024-04-29 02:31:43 +0700

    add img url scrape

--- detail.py
+++ detail.py
@@ -4,9 +4,9 @@ import json
 from bs4 import BeautifulSoup
 
 
-#url = "https://www.javdatabase.com/idols/hitomi-hitomi-tanaka/"
-with open ('link.txt', 'r') as f:
-    urls = [line.strip() for line in f]
+url = "https://www.javdatabase.com/idols/maria-ozawa/"
+# with open ('link.txt', 'r') as f:
+#     urls = [line.strip() for line in f]
 
 profile = {}
 codeList = []
@@ -15,6 +15,7 @@ soup = BeautifulSoup(r.text, "html.parser")
 
 idolProfile = soup.find('div', class_= 'col-12 col-xxl-7 col-xl-7 col-lg-7 col-md-12 col-sm-12')
 idolVideos = soup.find('div', class_= 'facetwp-template').find('div', class_='row')
+idolPhoto = soup.find('div', class_= 'idol-portrait').find('img')['src']
 
 
 def extract_data_profile(text):
@@ -33,24 +34,22 @@ def extract_data_profile(text):
 
 def extract_data_videos():
     for video in idolVideos:
-        code = video.find('a', class_='cut-text').text
+        code = video.find('a', class_='cut-text').text.strip(' ')
         codeList.append(code)
 
 
-scrape = ['Age:', 'DOB:', 'Birthplace:', 'Sign:', 'Blood:', 'Measurements:', 'Cup:', 'Height:', 'Shoe Size:']
-label = ['age', 'dob', 'birthplace', 'zodiac', 'blood', 'size3', 'cup', 'height', 'shoe']
+scrape = ['Age:', 'DOB:', 'Birthplace:', 'Sign:', 'Blood:', 'Measurements:', 'Cup:', 'Height:', 'Shoe Size:', 'Hair Length(s):', 'Hair Color(s):']
+label = ['age', 'dob', 'birthplace', 'zodiac', 'blood', 'size3', 'cup', 'height', 'shoe', 'hairlength', 'haircolor']
 profile['name'] = idolProfile.find('h1', class_='idol-name').text.strip(" - JAV Profile")
+profile['img'] = idolPhoto
 
-for i in range(0, 9):
+for i in range(0, 11):
     profile[label[i]] = extract_data_profile(scrape[i])
 
 extract_data_videos()
 profile['code'] = codeList
 print(profile)
 
-
-
-# print(links)
 
 with open("result.json", "w") as f:
     json.dump(profile, f)
